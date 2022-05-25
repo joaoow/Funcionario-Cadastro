fdb5cc21680b1744ea41eca11ce748a4ce07554e
joaolinux <87071803+joaolinux@users.noreply.github.com>
2022-05-25 07:56:49 -0300

    CadastroFuncionario
    Tela cadastrar e mostrar

---
+++ FuncionarioCadastro/src/modelo/Produto.java
@@ -0,0 +1,98 @@
+package modelo;
+
+import java.sql.Date;
+
+public class Produto {
+
+    private int codigo;
+    private String nome;
+    private long telefone;
+    private String email;
+    private Date datan; //variavel Date
+    private String rua;
+    private int numero;
+    private String cidade;
+    private String uf;
+    private String observacao;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDatan() {
+        return datan;
+    }
+
+    public void setDatan(Date datan) {
+        this.datan = datan;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    
+}
